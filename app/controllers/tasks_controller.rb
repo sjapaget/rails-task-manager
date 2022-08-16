e2b3a03eb93fe26b81911c1d7a43aacb1d439acc
Samuel Paget <sjapaget@gmail.com>
2022-08-16 12:45:21 +0200

    Add delete task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,12 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def set_task
