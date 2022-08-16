c6f1aeaabc9380ed567635d799ffd302efbeeba3
Samuel Paget <sjapaget@gmail.com>
2022-08-16 12:35:01 +0200

    Add task edit functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -18,9 +20,20 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
